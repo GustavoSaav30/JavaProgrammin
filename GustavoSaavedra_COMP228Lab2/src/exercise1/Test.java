3f161825ffd350ed335a012e6cd911097b790d96
GustavoSaav30 <gsaaved2@my.centennialcollege.ca>
2024-09-27 14:32:09 -0400

    generateMessage() in test class

--- GustavoSaavedra_COMP228Lab2/src/exercise1/Test.java
+++ GustavoSaavedra_COMP228Lab2/src/exercise1/Test.java
@@ -1,6 +1,9 @@
 package exercise1;
 
+import java.util.Random;
+
 public class Test {
+    Random random = new Random();
 
     public void simulateQuestion() {
 
@@ -8,8 +11,26 @@ public class Test {
     public void checkAnswer() {
 
     }
-    public void generateMessage() {
+    public String generateMessage(boolean isCorrect) {
+        int index = random.nextInt(4); // Generate a random number between 0 and 3
+        String response = "";
 
+        switch (index) {
+            case 0:
+                response = isCorrect ? "Excellent!" : "No. Please try again.";
+                break;
+            case 1:
+                response = isCorrect ? "Good!" : "Wrong. Try once more.";
+                break;
+            case 2:
+                response = isCorrect ? "Keep up the good work!" : "Don't give up.";
+                break;
+            case 3:
+                response = isCorrect ? "Nice work!" : "No. Keep trying.";
+                break;
+        }
+
+        return response;
     }
     public void inputAnswer() {
 
