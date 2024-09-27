824cf00896ed485f42324d03b18511aa7a4bfc8b
GustavoSaav30 <gsaaved2@my.centennialcollege.ca>
2024-09-26 23:07:47 -0400

    User class with set name overloaded

--- GustavoSaavedra_COMP228Lab2/src/exercise3/User.java
+++ GustavoSaavedra_COMP228Lab2/src/exercise3/User.java
@@ -3,5 +3,17 @@ package exercise3;
 public class User {
     String  name;
 
-    
+    public void setName(String firstName) {
+        this.name = firstName;
+    }
+    public void setName(String firstName, String lastName) {
+        this.name = String.format("%s %s", firstName, lastName);
+    }
+    public void setName(String firstName, String lastName, String middleName) {
+        this.name = String.format("%s %s %s", firstName, middleName, lastName);
+    }
+
+    public String getName() {
+        return name;
+    }
 }
