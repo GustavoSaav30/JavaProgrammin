92521db8247e3218144b8ca0e2ff742fbab24ab9
GustavoSaav30 <gsaaved2@my.centennialcollege.ca>
2024-09-27 13:02:20 -0400

    Test class

---
+++ GustavoSaavedra_COMP228Lab2/src/exercise1/Test.java
@@ -0,0 +1,18 @@
+package exercise1;
+
+public class Test {
+
+    public void simulateQuestion() {
+
+    }
+    public void checkAnswer() {
+
+    }
+    public void generateMessage() {
+
+    }
+    public void inputAnswer() {
+
+    }
+
+}
