554a60ad4aeb55069fd29a16b80bcb106cc92c01
GustavoSaav30 <gsaaved2@my.centennialcollege.ca>
2024-10-17 19:49:39 -0400

    MortgageConstants

---
+++ GustavoSaavedra_COMP228Lab3/src/exercise3/MortgageConstants.java
@@ -0,0 +1,9 @@
+package exercise3;
+
+public interface MortgageConstants {
+    int SHORT = 1;
+    int MEDIUM = 3;
+    int LONG = 5;
+    String BANK_NAME = "City Toronto Bank";
+    double MAX_MORTGAGE = 300000;
+}
